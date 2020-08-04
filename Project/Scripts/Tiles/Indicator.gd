30deb3db64adb16732877fa25b2ea3cb69da5508
Robert <bobbyras90@gmail.com>
2020-08-03 17:15:51 -0700

    Add better indicator tweening

--- Project/Scripts/Tiles/Indicator.gd
+++ Project/Scripts/Tiles/Indicator.gd
@@ -3,7 +3,6 @@ extends Node2D
 const _PASSIVE_ALPHA = 0.3
 const _ACTIVE_ALPHA = 1.0
 
-const _SELECT_GROWTH = Vector2(1.05, 1.05)
 const _SELECT_GROWTH_DURATION = 0.5
 
 enum State { DISABLED, OPEN, ALLY, BADDY, REACHABLE, PATH }
@@ -11,26 +10,31 @@ var state
 
 var _selected = false
 var _color
+var _select_color
 
 func select(new_select):
 	if new_select == _selected:
 		return
 	_selected = new_select
 	if _selected:
+		if state == State.DISABLED:
+			_select_color = Color.red
+		else:
+			_select_color = Color.white
 		_run_selected()
 	else:
 		$HoveredIndicatorTween.stop_all()
 		$HoveredIndicatorTween.remove_all()
-		$HoveredIndicatorTween.interpolate_property(self, "scale", scale, Vector2(1,1), 0.5, Tween.TRANS_QUAD, Tween.EASE_OUT)
-		$HoveredIndicatorTween.interpolate_property(self, "modulate", modulate, _color, 0.5, Tween.TRANS_QUAD, Tween.EASE_OUT)
+		$HoveredIndicatorTween.interpolate_property(self, "modulate", modulate, _color, 0.5, Tween.TRANS_CUBIC, Tween.EASE_OUT)
 		$HoveredIndicatorTween.start()
+		
 
 func set_state(new_state):
 	state = new_state
 	
 	match(state):
 		State.DISABLED:
-			_color = Color.black
+			_color = Color.white
 			_color.a = 0
 		State.OPEN:
 			_color = Color.white
@@ -57,15 +61,12 @@ func clear_pathing():
 
 func _run_selected():
 	while _selected:
-		$HoveredIndicatorTween.interpolate_property(self, "scale", Vector2(1,1), _SELECT_GROWTH, _SELECT_GROWTH_DURATION / 3.0, Tween.TRANS_CUBIC, Tween.EASE_OUT)
-		$HoveredIndicatorTween.interpolate_property(self, "modulate", _color, Color.white, _SELECT_GROWTH_DURATION / 3.0, Tween.TRANS_CUBIC, Tween.EASE_OUT)
+		$HoveredIndicatorTween.interpolate_property(self, "modulate", _color, _select_color, _SELECT_GROWTH_DURATION / 3.0, Tween.TRANS_CUBIC, Tween.EASE_OUT)
 		$HoveredIndicatorTween.start()
 		yield($HoveredIndicatorTween, "tween_all_completed")
 		if _selected:
-			$HoveredIndicatorTween.interpolate_property(self, "scale", _SELECT_GROWTH, Vector2(1,1), _SELECT_GROWTH_DURATION, Tween.TRANS_QUAD, Tween.EASE_IN)
-			$HoveredIndicatorTween.interpolate_property(self, "modulate", Color.white, _color, _SELECT_GROWTH_DURATION, Tween.TRANS_QUAD, Tween.EASE_IN)
+			$HoveredIndicatorTween.interpolate_property(self, "modulate", _select_color, _color, _SELECT_GROWTH_DURATION, Tween.TRANS_QUAD, Tween.EASE_IN)
 			$HoveredIndicatorTween.start()
 			yield($HoveredIndicatorTween, "tween_all_completed")
-	$HoveredIndicatorTween.interpolate_property(self, "scale", scale, Vector2(1,1), 0.1, Tween.TRANS_QUAD, Tween.EASE_OUT)
-	$HoveredIndicatorTween.interpolate_property(self, "modulate", modulate, _color, 0.1, Tween.TRANS_QUAD, Tween.EASE_OUT)
-	$HoveredIndicatorTween.start()
+	#$HoveredIndicatorTween.interpolate_property(self, "modulate", modulate, _color, 0.1, Tween.TRANS_QUAD, Tween.EASE_OUT)
+	#$HoveredIndicatorTween.start()
