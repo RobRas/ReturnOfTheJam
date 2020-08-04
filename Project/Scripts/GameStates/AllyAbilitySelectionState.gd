c547644766eaf2cd01d1d5dacc0ebe13b43f32f0
Robert <bobbyras90@gmail.com>
2020-08-03 21:57:06 -0700

    unstash

--- Project/Scripts/GameStates/AllyAbilitySelectionState.gd
+++ Project/Scripts/GameStates/AllyAbilitySelectionState.gd
@@ -3,10 +3,21 @@ extends Node2D
 export(NodePath) var player_select_state_path
 var _player_select_state
 
+var _ally
+
 var enabled = false
 
 func _ready():
 	_player_select_state = get_node(player_select_state_path)
 
-func enter(_ally):
-	_player_select_state.enter()
+func _process(delta):
+	if not enabled:
+		return
+	
+	if Input.is_action_just_pressed("skip_ability"):
+		enabled = false
+		_player_select_state.enter()
+
+func enter(ally):
+	_ally = ally
+	enabled = true
